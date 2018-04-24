f5bbd81b3f57734760679a6441f1627579d279b5
FrankieMarie <frankiemarie83@gmail.com>
2018-04-23 18:42:41 -0700

    display project info in user gallery

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,13 @@ const knex = require("../db/knex.js");
 
 module.exports = {
   bio: (req, res)=>{
-    knex('users')
-      .where('id', req.params.id)
-      .then((data)=>{
-        res.render('gallery', {user: data[0]})
+    knex('users').where('id', req.params.id)
+    .then((data)=>{
+      knex('projects').where('projects.user_id', req.params.id)
+      .join('images', 'images.project_id', 'projects.id')
+      .then((projects)=>{
+          res.render('gallery', {user: data[0], projects: projects})
       })
+    })
   }
 }
